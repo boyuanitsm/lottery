8ea4856c62d44b46a5c601c4b060d6cd41dea288
hookszhang <hua.zhang@boyuanitsm.com>
2017-01-06 15:08:54 +0800

    Start and stop in the lottery

--- src/app/lottery.js
+++ src/app/lottery.js
@@ -3,10 +3,28 @@ import trophies from '../trophy';
 
 export default {
   template: require('./lottery.html'),
-  controller() {
+  controller($log, $interval, $scope) {
     'ngInject';
 
     this.staffs = staffs;
     this.trophies = trophies;
+    let timeout = 0;
+    let interval;
+
+    $scope.$watch('$ctrl.inTheLottery', (newValue, oldValue, event) => {
+      if (newValue) {
+        interval = $interval(() => {
+          timeout++;
+          this.luckyDog = staffs[timeout % staffs.length];
+        }, 100);
+      } else {
+        $interval.cancel(interval)
+      }
+      return newValue;
+    });
+
+    $scope.$on('$destroy', function() {
+      $interval.cancel(interval);
+    });
   }
 };
