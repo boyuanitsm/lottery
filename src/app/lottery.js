e3c2b0bea5bac7c42b204346fcc1fc77c9304271
hookszhang <hua.zhang@boyuanitsm.com>
2017-01-06 14:35:12 +0800

    Add staff and trophy json

--- src/app/lottery.js
+++ src/app/lottery.js
@@ -1,5 +1,12 @@
-export const hello = {
+import staffs from '../staff';
+import trophies from '../trophy';
+
+export default {
   template: require('./lottery.html'),
   controller() {
+    'ngInject';
+
+    this.staffs = staffs;
+    this.trophies = trophies;
   }
 };
--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import angular from 'angular';
 
-import {hello} from './app/hello';
+import lottery from './app/lottery';
 import 'angular-ui-router';
 import routesConfig from './routes';
 
@@ -11,4 +11,4 @@ export const app = 'app';
 angular
   .module(app, ['ui.router'])
   .config(routesConfig)
-  .component('app', hello);
+  .component('app', lottery);
