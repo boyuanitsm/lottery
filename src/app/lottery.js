2a146ef07f99058a62c554743b9e2f5c1ba96767
hookszhang <hua.zhang@boyuanitsm.com>
2017-01-09 10:20:18 +0800

    Add luckey dog to cookies

--- src/app/lottery.js
+++ src/app/lottery.js
@@ -2,26 +2,60 @@ import staffs from '../staff';
 import trophies from '../trophy';
 
 const LOTTERY_INTERVAL = 10;
+const LUCKEY_DOG = 'LUCKEY_DOG';
+const TROPHY_ISSUED = 'TROPHY_ISSUED';
 
 export default {
   template : require('./lottery.html'),
-  controller($log, $interval, $scope) {
+  controller($log, $interval, $scope, $cookies) {
     'ngInject';
 
-    this.staffs = staffs;
-    this.trophies = trophies;
+    const vm = this;
     let interval;
+
+    vm.staffs = staffs;
+    vm.trophies = trophies;
+    vm.reset = reset;
+
+    initialize();
+
+    function initialize() {
+      vm.luckeyDogs = $cookies.get(LUCKEY_DOG)
+        ? angular.fromJson($cookies.get(LUCKEY_DOG))
+        : [];
+      vm.trophyIssued = $cookies.get(TROPHY_ISSUED)
+        ? angular.fromJson($cookies.get(TROPHY_ISSUED))
+        : {};
+    }
+
+    function readyTrophy() {
+
+    }
+
+    function addLuckyDog(name, trophy) {
+      vm.luckeyDogs[vm.luckeyDogs.length] = {
+        name: name,
+        trophy: trophy
+      }
+      $cookies.put(LUCKEY_DOG, angular.toJson(vm.luckeyDogs));
+    }
+
+    function reset() {
+      $cookies.remove(LUCKEY_DOG);
+      $cookies.remove(TROPHY_ISSUED);
+      vm.luckeyDogs = [];
+      vm.trophyIssued = {};
+    };
 
     $scope.$watch('$ctrl.inTheLottery', (newValue, oldValue, event) => {
       if (newValue) {
         interval = $interval(() => {
-          this.luckyDog = staffs[random(0, staffs.length)];
+          vm.luckyDog = staffs[random(0, staffs.length)];
         }, LOTTERY_INTERVAL);
       } else {
         if (interval) {
           $interval.cancel(interval)
-          // const luckDogIndex = random(0, staffs.length);
-          // this.luckyDog = staffs[luckDogIndex];
+          addLuckyDog(vm.luckyDog.name, trophies[0].id);
         }
       }
       return newValue;
