dcf64392a831f19e11e2ff4f94b1c2007b1aa4d0
hookszhang <hua.zhang@boyuanitsm.com>
2017-01-10 12:13:45 +0800

    Remove unused ng cookies

--- src/cookie.config.js
+++
@@ -1,9 +0,0 @@
-// cookie 有效期为七天
-export default function($cookiesProvider) {
-  'ngInject';
-
-  let now = new Date()
-  now.setDate(now.getDate() + 7);
-
-  $cookiesProvider.defaults.expires = now;
-}
--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import angular from 'angular';
 
 import lottery from './app/lottery';
 import 'angular-ui-router';
-import 'angular-cookies';
 import 'angular-local-storage';
 import routesConfig from './routes';
-import cookieConfig from './cookie.config';
 import luckyDog from './app/lucky-dog';
 
 import './index.scss';
@@ -13,8 +11,7 @@ import './index.scss';
 export const app = 'app';
 
 angular
-  .module(app, ['ui.router', 'ngCookies', 'LocalStorageModule'])
+  .module(app, ['ui.router', 'LocalStorageModule'])
   .config(routesConfig)
-  .config(cookieConfig)
   .component('app', lottery)
   .component('luckyDog', luckyDog);
